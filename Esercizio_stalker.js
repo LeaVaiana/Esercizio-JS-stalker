17ac479172fbf9307ee2acbe0b7abbc4e48a0360
Lea Vaiana <leavaiana@gmail.com>
2018-11-14 15:11:26 +0100

    Files added

---
+++ Esercizio_stalker.js
@@ -0,0 +1,5 @@
+var firstName = prompt("What's your first name");
+var lastName = prompt("What's your last name");
+var age = prompt("What's your age");
+console.log("My full name is "+ firstName + " " + lastName);
+console.log("My age is "+ age + " years old");
