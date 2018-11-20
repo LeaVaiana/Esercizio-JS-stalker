8c8cd343552f470b393c8c9d7f269f69ed057746
Lea Vaiana <leavaiana@gmail.com>
2018-11-20 17:38:09 +0100

    Added alert for viewing exercize in browser

--- Esercizio_stalker.js
+++ Esercizio_stalker.js
@@ -3,3 +3,6 @@ var lastName = prompt("What's your last name");
 var age = prompt("What's your age");
 console.log("My full name is "+ firstName + " " + lastName);
 console.log("My age is "+ age + " years old");
+alert("My full name is "+ firstName + " " + lastName);
+alert("My age is "+ age + " years old");
+
